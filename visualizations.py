d3dd69b0bc1c95fd804e2e2ea69e347626a8ece6
Will Barden <wfbarden@gmail.com>
2023-07-10 23:28:25 -0400

    Cleanup

--- visualizations.py
+++ visualizations.py
@@ -1,3 +1,5 @@
+import os
+from functools import reduce
 from typing import Optional
 
 import networkx as nx
@@ -7,7 +9,7 @@ from catalog import Catalog
 from type_defs import *
 
 
-def create_crafting_graph(catalog):
+def create_crafting_graph(catalog: Catalog):
   base_item_color = 'lightgreen'
   item_color = 'lightblue'
   edge_color = 'lightgray'
@@ -23,9 +25,9 @@ def create_crafting_graph(catalog):
   for recipe in catalog.recipes:
     recipe_id = str(hash(recipe))
     if recipe.time:
-      label = str(recipe.time) + 's'
+      label = str(catalog.building_types[recipe.building]) + ' ' + str(recipe.time) + 's'
     elif recipe.fraction:
-      label = str(recipe.fraction * 100) + '%'
+      label = str(catalog.building_types[recipe.building]) + ' ' + str(recipe.fraction * 100) + '%'
     G.add_node(recipe_id, label=label, shape='text', color=edge_color)
     for product, product_quantity in recipe.products.items():
       G.add_edge(recipe_id, product, label=str(product_quantity), color=edge_color)
@@ -33,7 +35,7 @@ def create_crafting_graph(catalog):
         G.add_edge(material, recipe_id, label=str(material_quantity), color=edge_color)
   return G
 
-def create_dependency_graph(catalog):
+def create_dependency_graph(catalog: Catalog):
   base_item_color = 'lightgreen'
   item_color = 'lightblue'
   edge_color = 'lightgray'
@@ -47,42 +49,34 @@ def create_dependency_graph(catalog):
     G.add_node(matrix.id, label=matrix.name, color=matrix.color)
 
   for recipe in catalog.recipes:
-    for product, _ in recipe.products.items():
+    for product, product_quantity in recipe.products.items():
       for material, material_quantity in recipe.materials.items():
-        G.add_edge(product, material, label=str(material_quantity), color=edge_color)
+        G.add_edge(product, material, label=str(material_quantity), color=edge_color, 
+                   material_quantity=material_quantity, product_quantity=product_quantity, time=recipe.time)
   return G
-  
-
-def highlight_dependencies(graph: nx.DiGraph, target: str, parent: Optional[str] =None):
-  for n in graph.neighbors(graph.nodes[target]):
-    print(n)
 
 
-def create_item_dependency_graph(catalog, item):
-  base_color = 'lightgreen'
-  item_color = 'lightblue'
-  edge_color = 'lightgray'
-  highlight_color = 'orange'
-
-  graph = create_dependency_graph(catalog)
-  descendants = nx.descendants(graph, item) | {item}
-  graph = graph.subgraph(descendants)
-  return graph
-
-
-def generate_html(G, file_name):
+def generate_html(graph, file_name):
   net = Network(directed=True, height='98vh', font_color='black', layout=False)
   net.hrepulsion(node_distance=100, central_gravity=0.05, spring_length=200, spring_strength=0.01)
   net.set_edge_smooth('continuous')
-  net.from_nx(G)
+  net.from_nx(graph)
   net.save_graph(file_name)
 
 
 def main():
   catalog = Catalog.load()
   catalog.validate()
-  graph = create_item_dependency_graph(catalog, 'electric-motor')
-  generate_html(graph, 'dsp_crafting_tree.html')
+
+  visualization_dir = 'dsp_diagrams'
+  if not os.path.exists(visualization_dir):
+    os.mkdir(visualization_dir)
+
+  for name, method in {
+    'crafting_graph': [create_crafting_graph, catalog],
+    'dependency_graph': [create_dependency_graph, catalog]
+  }.items():
+    generate_html(method[0](*method[1:]), os.path.join(visualization_dir, name) + '.html')
 
 
 if __name__ == '__main__':
